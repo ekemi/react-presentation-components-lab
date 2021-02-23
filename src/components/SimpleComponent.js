e6c8656ef951ef9955806e7f6689008bb2625276
Yapnounkie <edmundopierre@yahoo.com>
2021-02-23 09:32:24 -0800

    fix minor error

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,38 @@
 // Code SimpleComponent Here
+import React, { Component } from 'react'
+
+export default class SimpleComponent extends Component {
+constructor(){
+    super()
+    this.state = {
+        mood: "happy"
+    }
+    //I have to bind because setState could not be read
+    this.handleToggleClick = this.handleToggleClick.bind(this)
+
+}
+   
+
+    handleToggleClick () {
+     const {mood} = this.state;
+     if (mood === 'happy'){
+        this.setState({
+            mood: 'sad'
+        })
+
+     }else{
+         this.setState({
+             mood: 'happy'
+         })
+     }
+
+    }
+    render() {
+        return (
+            <div onClick={this.handleToggleClick}>
+         {this.state.mood}
+                
+            </div>
+        )
+    }
+}
--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,15 @@
 // Code SimplerComponent Here
+import React from 'react'
+
+function SimplerComponent({handleClick}) {
+
+    
+    return (
+       
+        <div onClick={handleClick}>
+            <button>I am just happy</button>
+        </div>
+    )
+}
+
+export default SimplerComponent
